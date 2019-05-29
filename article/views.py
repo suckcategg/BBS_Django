ed85fcadd3e1d360b08b3a8ef22aaf242f38e916
suckcategg <1254454694@qq.com>
2019-05-29 17:33:36 +0800

    /article/article-detail/1/ BUG已解决(文件路径创建出错)

--- article/views.py
+++ article/views.py
@@ -9,7 +9,7 @@ def article_list(request):
     context = {'articles':article}
     return render(request,'article/list.html',context)
 
-def article_detail(request):
+def article_detail(request,id):
     article = ArticlePost.objects.get(id=id)
     context = {'article': article}
-    return  render(request, 'article/detail.html',context)
+    return  render(request, 'article/detail.html', context)
