865a6cdd146e7ec86a56fba5604f2654eefc6680
12544 <1254454694@qq.com>
2019-05-30 22:29:24 +0800

    完成登录/退出

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse
+from .forms import UserLoginForm
 
 # Create your views here.
+
+
+def user_login(request):
+    if request.method == 'POST':
+        user_login_form = UserLoginForm(data=request.POST)
+        if user_login_form.is_valid():
+            data = user_login_form.cleaned_data
+            # 检验账号、密码是否正确匹配数据库中的某个用户
+            # 如果均匹配则返回这个 user 对象
+            user = authenticate(username=data['username'], password=data['password'])
+            if user:
+                login(request, user)
+                return redirect("article:article_list")
+            else:
+                return HttpResponse("账号或密码输入错误，请重新输入！")
+        else:
+            return HttpResponse("账号或密码输入不合法")
+    elif request.method == 'GET':
+        user_login_form = UserLoginForm()
+        context = {'from': user_login_form}
+        return render(request, 'userprofile/login.html', context)
+    else:
+        return HttpResponse("请使用GET或POST请求数据")
+def user_logout(request):
+    logout(request)
+    return redirect("article:article_list")
