43cad54483a3ddc4ae22246dc4323fbefcc6730e
suckcategg <1254454694@qq.com>
2019-05-31 15:38:28 +0800

    完成"重置密码"   邮箱配置已注释

--- django_BBS/urls.py
+++ django_BBS/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('article/', include('article.urls', namespace='article')),
     path('userprofile/', include('userprofile.urls', namespace='userprofile')),
-
+    path('password-reset/', include('password_reset.urls')),
 ]
