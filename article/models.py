8fd4b5a477945d563a087d3734522b817b5ad717
suckcategg <1254454694@qq.com>
2019-06-06 17:56:27 +0800

    数据库迁移到mysql

--- article/models.py
+++ article/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 
 
+
 class ArticlePost(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
--- django_BBS/settings.py
+++ django_BBS/settings.py
@@ -80,17 +80,27 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_BBS.wsgi.application'
 
-
+#
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'BBS',
+        'HOST': '47.102.131.85',
+        'PORT':  3306,
+        'USER': 'root',
+        'PASSWORD': 'toor',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
