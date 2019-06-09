448bd746f22b6d0601570674c001043646f7e987
12544 <1254454694@qq.com>
2019-06-09 17:57:05 +0800

    get请求注册页面不传data

--- django_BBS/urls.py
+++ django_BBS/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.conf.urls import url,include
+from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +27,4 @@ urlpatterns = [
     path('password-reset/', include('password_reset.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
